43847b757947d7c041cd5d28528af2f41a1b619d
Justin <jcdiner@gmail.com>
2023-03-23 16:28:14 -0400

    cartReducer

---
+++ src/store/cart.js
@@ -0,0 +1,26 @@
+const POPULATE_CART = ''
+
+export default function cartReducer(state = {}, action) {
+	return state; 
+} 
+
+export function populateCart(produce.id) {
+	return {
+		type: 
+		produce.id: 
+	}
+}
+
+//export default function produceReducer(state = {}, action) {
+
+//	switch (action.type) {
+//			case POPULATE:
+//				// Normalizing Data!
+//				const newState = {};
+//				action.produce.forEach(produce => {
+//				}) 
+//				return newState
+//			default:
+//				return state;
+//	}
+//}
